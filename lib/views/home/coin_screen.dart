62ffa4a2987aec2eb1ecd4f0962c2b4e7b354d9b
Henrique Dias <henriquenobrega2002@gmail.com>
2023-09-15 17:32:16 -0300

    Updated UI and Added pull down for refreshing

--- lib/views/home/coin_screen.dart
+++ lib/views/home/coin_screen.dart
@@ -22,7 +22,7 @@ class CoinScreen extends StatefulWidget {
 class _CoinScreenState extends State<CoinScreen> {
   
   int index = 0;
-  
+  int components = 1;
   _CoinScreenState(index);
   
   
@@ -34,6 +34,9 @@ class _CoinScreenState extends State<CoinScreen> {
     
   }
 
+
+  
+
   @override
   Widget build(BuildContext context) {
     
@@ -41,99 +44,14 @@ class _CoinScreenState extends State<CoinScreen> {
     Color percentageColor = priceChange24H < 0 ?  Colors.red: const Color.fromARGB(255, 0, 255, 8);
     double price = controller.coinsList[widget.index].currentPrice;
 
-    
-
-    return  Scaffold(
-      appBar: AppBar(
-        backgroundColor: Color.fromARGB(246, 115, 255, 0),
-        title: Text('Go Back',style: textStyle(18,Colors.black,FontWeight.bold)),
-      ),
-      backgroundColor:  Colors.white,
-      body: Center(
-        child: Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.only(top: 10.0),
-              child: Container(
-                width: MediaQuery.of(context).size.width*0.6,
-                height: MediaQuery.of(context).size.height*0.4 + 3.2,
-                
-                decoration:  BoxDecoration(
-                  borderRadius: BorderRadius.circular(20),
-                  gradient: const LinearGradient(
-                    begin: Alignment.centerLeft,
-                    end: Alignment.centerRight,
-                    colors: [
-                   Color.fromARGB(255, 4, 255, 12),Color.fromARGB(255, 187, 255, 0)
-                  ]),
-                  boxShadow: const [BoxShadow(
-                      color:  Color.fromARGB(255, 179, 255, 0),
-                      offset: Offset(6, 6),
-                      blurRadius: 5
-                  )]
-            
-                ),
-                child: Padding(
-                  padding: const EdgeInsets.all(20.0),
-                  child: Column(
-                    
-                    children: [
-                      Image.network(
-                      controller.coinsList[widget.index].image),
-                      FittedBox(
-                        fit: BoxFit.fitWidth,
-                        child: Text(controller.coinsList[widget.index].name,style: textStyle(45,Colors.white, FontWeight.w900),
-                        )
-                      ),
-                    ],
-                  ),
-                ),
-                
-              ),
-            ),
-            Container(
-              
-              child: Padding(
-                      padding: const EdgeInsets.all(40.0),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Row(
-                            children: [
-                              Text('Price: ',
-                              style: textStyle(20, Colors.grey, FontWeight.w700)
-                              ),
-                              Text('US\$ ${price}',
-                              style: textStyle(20, Colors.black, FontWeight.w700)
-                              ),
-                            ],
-                          ),
-                          Row(
-                            children: [
-                              Text('Changed: ',
-                              style: textStyle(20, Colors.grey, FontWeight.w700)
-                              ),
-                              Text(priceChange24H> 0 ?'+${priceChange24H} %' :'${priceChange24H} %',
-                              style: textStyle(20, percentageColor, FontWeight.w700)
-                              ),
-                            ],
-                          ),
-                   
-                          Center(
-                            child: Obx(
-                               () => controller.isLoading.value ? const Center(child:CircularProgressIndicator()): 
-                               Padding(
-                                 padding: const EdgeInsets.only(top: 18.0),
-                                 child: OutlinedButton(
-                                  
-                                  onPressed: () {
-                                    if(controller.inCooldown){
-                                      showDialog(
-                                      context: context,
-                                      builder: (context) {
+    Future refresh () async {
+        if(controller.inCooldown){
+          showDialog(
+          context: context,
+          builder: (context) {
                                         return AlertDialog(
                                                 title: Text("Error!"),
-                                                content: Text("an error with the api has ocurred!"),
+                                                content: Text("an error has ocurred!"),
                                                   actions: [
                                                   ElevatedButton(
                                                     onPressed: () {
@@ -147,24 +65,159 @@ class _CoinScreenState extends State<CoinScreen> {
                                             );
                                     }else{
                                       setState((){
+                                        developer.log('refreshing');
                                         controller.fetchCoins();
                                         priceChange24H = controller.coinsList[widget.index].marketCapChangePercentage24H;
                                         price = controller.coinsList[widget.index].currentPrice;
                                       });
                                     }
-                                  },
-                                   style: OutlinedButton.styleFrom( backgroundColor: Color.fromARGB(255, 21, 255, 0),),
-                                   child: Text('Reload',style: textStyle(28, Colors.white, FontWeight.w700),)
-                                                           ),
-                               ),
-                            ),
-                          )
-                        ],
-                      ),
-                      
+    }
+
+    
+
+    return  Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color.fromARGB(255, 160, 255, 131),
+        title: Text('Go Back',style: textStyle(18,Colors.white,FontWeight.w300)),
+      ),
+      backgroundColor:  Colors.white,
+      body: Center(
+        child: RefreshIndicator(
+          onRefresh: refresh,
+          child: ListView.builder(
+            itemCount: 1,
+            itemBuilder: (context,_){
+              
+              return Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(top: 10.0),
+                  child: Container(
+                    width: MediaQuery.of(context).size.width*0.6,
+                    height: MediaQuery.of(context).size.height*0.3 + 3.2,
+                    
+                    decoration:  BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      gradient: const LinearGradient(
+                        begin: Alignment.centerLeft,
+                        end: Alignment.centerRight,
+                        colors: [
+                       Color.fromARGB(255, 4, 255, 12),Color.fromARGB(255, 187, 255, 0)
+                      ]),
+                      boxShadow: const [BoxShadow(
+                          color:  Color.fromARGB(255, 179, 255, 0),
+                          offset: Offset(6, 6),
+                          blurRadius: 5
+                      )]
+                
                     ),
-            )
-          ],
+                    child: FittedBox(
+                      child: Padding(
+                        padding: const EdgeInsets.all(20.0),
+                        child: Column(
+                          
+                          children: [
+                            Image.network(
+                            controller.coinsList[widget.index].image),
+                            FittedBox(
+                              fit: BoxFit.fitWidth,
+                              child: Text(controller.coinsList[widget.index].name,style: textStyle(45,Colors.white, FontWeight.w500),
+                              )
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    
+                  ),
+                ),
+                Container(
+                  
+                  child: Padding(
+                          padding: const EdgeInsets.all(40.0),
+                          child: IntrinsicHeight(
+                            child: Container(
+                              height: 100,
+                              child: Row(
+                                
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Column(
+                                    children: [
+                                      Container(
+                                        alignment: Alignment.center,
+                                        width: 100,
+                                        child: Text('Price ',
+                                        style: textStyle(20, Colors.grey, FontWeight.w300)
+                                        ),
+                                      ),
+                                      Container(
+                                        width: 100,
+                                        child: Obx (() => controller.isLoading.value ? const Center(child:CircularProgressIndicator()):
+                                          FittedBox(
+                                            fit: BoxFit.contain,
+                                            child: Text('US\$ ${price}',
+                                            style: textStyle(20, Colors.black, FontWeight.w300)
+                                            ),
+                                          ),
+                                        ),
+                                      ),
+                                                      
+                                    ],
+                                  ),
+                                  const VerticalDivider(
+                                    
+                                    thickness: 1,
+                                    color: Colors.grey,
+                                    ),
+                                  Column(
+                                    children: [
+                                      Container(
+                                        alignment: Alignment.center,
+                                        width: 100,
+                                        child: Text('Changed ',
+                                        style: textStyle(20, Colors.grey, FontWeight.w300)
+                                        ),
+                                      ),
+                                      Container(
+                                        
+                                        width: 100,
+                                        child: FittedBox(
+                                          alignment: Alignment.center,
+                                          fit: BoxFit.contain,
+                                          child: Text(priceChange24H> 0 ?'+${priceChange24H} %' :'${priceChange24H} %',
+                                          style: textStyle(20, percentageColor, FontWeight.w300)
+                                          ),
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                            
+                                ],
+                              ),
+                            ),
+                          ),
+                          
+                          
+                        ),
+                ),
+                Center(
+                                child: Obx(
+                                   () => controller.isLoading.value ? const Center(child:CircularProgressIndicator()): 
+                                   Padding(
+                                     padding: const EdgeInsets.only(top: 18.0),
+                                     
+                                   ),
+                                ),
+                              )
+              ],
+            );
+            }
+
+            
+            
+            
+          ),
         ),
       ),
 
@@ -172,6 +225,8 @@ class _CoinScreenState extends State<CoinScreen> {
     );
     
   }
+
+  
 }
 
 
--- lib/views/home/home_screen.dart
+++ lib/views/home/home_screen.dart
@@ -15,22 +15,20 @@ class HomeScreen extends StatelessWidget {
 
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Color.fromARGB(246, 115, 255, 0),
-        title: Center(child: Text('Crypto Tracker',style: textStyle(25,Colors.black,FontWeight.bold))),
+        backgroundColor: Color.fromARGB(255, 160, 255, 131),
+        title: Center(child: Text('Crypto Tracker',style: textStyle(25,Colors.white,FontWeight.bold))),
       ),
       backgroundColor: Colors.white,
       body: Padding(
-        padding: const EdgeInsets.only(left: 20.0,right: 20,top:20),
+        padding: const EdgeInsets.only(left: 20.0,right: 20),
         child: SingleChildScrollView(
           physics: ScrollPhysics(),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Center(
-                child: Text(
-                  "Scroll down!",
-                  style:textStyle(25, Colors.black, FontWeight.bold),
-                ),
+              const SizedBox(
+                     height: 8
+                                              
               ),
               Obx(
                 ()=>  controller.isLoading.value
@@ -46,111 +44,125 @@ class HomeScreen extends StatelessWidget {
                     double priceChange24H = controller.coinsList[index].marketCapChangePercentage24H;
                     Color percentageColor = priceChange24H < 0 ?  Colors.red: const Color.fromARGB(255, 0, 255, 8);
 
-                    return Padding(
-                      padding: const EdgeInsets.only(right: 20.0, top:30),
-                      child: InkWell(
-                        borderRadius: BorderRadius.circular(20),
-                        onTap: (){
-                          var screen = CoinScreen(index: index);
-                          Navigator.of(context).push(_createRoute(screen));
-                        },
-                        child: Container(
-                                          
-                          width: MediaQuery.of(context).size.width,
-                          height: 60,
-                          
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Row(
-                                children: [
-                                  Container(
-                                    width: 60,
-                                    height: 60,
-                                    decoration: BoxDecoration(
-                                      gradient: LinearGradient(
-                                        begin: Alignment.centerLeft,
-                                        end: Alignment.centerRight,
-                                        colors: const[
-                                        Color.fromARGB(255, 4, 255, 12),Color.fromARGB(255, 187, 255, 0)
-                                      ]),
-                                      color: Colors.grey,
-                                      borderRadius: BorderRadius.circular(20),
-                                      boxShadow: const [BoxShadow(
-                                        color:  Color.fromARGB(255, 179, 255, 0),
-                                        offset: Offset(6, 6),
-                                        blurRadius: 5
-                                      )]
-                                    ),
-                                    child: Padding(
-                                      padding: const EdgeInsets.all(10),
-                                      child: Image.network(
-                                        controller.coinsList[index].image,
-                                      ),
-                                      
-                                    ),
-                                  ),
-                                  const SizedBox(
-                                    width: 20
-                                  ),
-                                  Container(
-                                    width:MediaQuery.of(context).size.width*0.3,
+                    return Column(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(right: 20.0),
+                          child: InkWell(
+                            borderRadius: BorderRadius.circular(20),
+                            onTap: (){
+                              var screen = CoinScreen(index: index);
+                              Navigator.of(context).push(_createRoute(screen));
+                            },
+                            child: Container(
+                                              
+                              width: MediaQuery.of(context).size.width,
+                              height: 60,
                               
-                                    child: Column(
-                                      crossAxisAlignment: CrossAxisAlignment.start,
-                                      
-                                      children: [
-                                        const SizedBox(
-                                          height: 8
-                                          
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Row(
+                                    children: [
+                                      Container(
+                                        width: 60,
+                                        height: 60,
+                                        decoration: BoxDecoration(
+                                          gradient: LinearGradient(
+                                            begin: Alignment.centerLeft,
+                                            end: Alignment.centerRight,
+                                            colors: const[
+                                            Color.fromARGB(255, 4, 255, 12),Color.fromARGB(255, 187, 255, 0)
+                                          ]),
+                                          color: Colors.grey,
+                                          borderRadius: BorderRadius.circular(20),
+                                          boxShadow: const [BoxShadow(
+                                            color:  Color.fromARGB(255, 179, 255, 0),
+                                            offset: Offset(2, 2),
+                                            blurRadius: 5
+                                          )]
+                                        ),
+                                        child: Padding(
+                                          padding: const EdgeInsets.all(10),
+                                          child: Image.network(
+                                            controller.coinsList[index].image,
                                           ),
-                                        Text(controller.coinsList[index].name,
-                                        style: textStyle(18, Colors.black, FontWeight.w600),
-                                        overflow: TextOverflow.fade,
-                                        softWrap: false,
-                                        maxLines: 1,
-                                        
+                                          
                                         ),
-                                        Text(priceChange24H> 0 ?'+${priceChange24H} %' :'${priceChange24H} %',
-                                        style: textStyle(18, percentageColor, FontWeight.w600),
-                                        overflow: TextOverflow.fade,
-                                        softWrap: false,
-                                        maxLines: 1,
+                                      ),
+                                      const SizedBox(
+                                        width: 20
+                                      ),
+                                      Container(
+                                        width:MediaQuery.of(context).size.width*0.3,
+                                  
+                                        child: Column(
+                                          crossAxisAlignment: CrossAxisAlignment.start,
+                                          
+                                          children: [
+                                            const SizedBox(
+                                              height: 8
+                                              
+                                              ),
+                                            Text(controller.coinsList[index].name,
+                                            style: textStyle(18, Colors.black, FontWeight.w500),
+                                            overflow: TextOverflow.fade,
+                                            softWrap: false,
+                                            maxLines: 1,
+                                            
+                                            ),
+                                            Text(priceChange24H> 0 ?'+${priceChange24H} %' :'${priceChange24H} %',
+                                            style: textStyle(18, percentageColor, FontWeight.w300),
+                                            overflow: TextOverflow.fade,
+                                            softWrap: false,
+                                            maxLines: 1,
+                                            ),
+                                            
+                                          ],
                                         ),
-                                        
+                                      ),
                                       ],
                                     ),
-                                  ),
-                                  ],
-                                ),
-                              
-                              Container(
-                                width: MediaQuery.of(context).size.width * 0.3,
-                                child: Column(
-                                  crossAxisAlignment: CrossAxisAlignment.end,
-                                  children: [
-                                    const SizedBox(
-                                          height: 8
-                                          ),
-                                    Text('\$${controller.coinsList[index].currentPrice}',
-                                    style: textStyle(18, Colors.white, FontWeight.w600),
-                                    overflow: TextOverflow.fade,
-                                    softWrap: false,
-                                    maxLines: 1,
+                                  
+                                  Container(
+                                    width: MediaQuery.of(context).size.width * 0.3,
+                                    child: Column(
+                                      crossAxisAlignment: CrossAxisAlignment.end,
+                                      children: [
+                                        const SizedBox(
+                                              height: 8
+                                              ),
+                                        Text('\$${controller.coinsList[index].currentPrice}',
+                                        style: textStyle(18, Colors.black, FontWeight.w200),
+                                        overflow: TextOverflow.fade,
+                                        softWrap: false,
+                                        maxLines: 1,
+                                        ),
+                                        Text(controller.coinsList[index].symbol.toUpperCase(),
+                                        style: textStyle(18, Colors.grey, FontWeight.w600),
+                                        overflow: TextOverflow.fade,
+                                        softWrap: false,
+                                        maxLines: 1,
+                                        )
+                                      ],
                                     ),
-                                    Text(controller.coinsList[index].symbol.toUpperCase(),
-                                    style: textStyle(18, Colors.grey, FontWeight.w600),
-                                    overflow: TextOverflow.fade,
-                                    softWrap: false,
-                                    maxLines: 1,
-                                    )
-                                  ],
-                                ),
-                              )
-                            ],  
+                                  )
+                                  
+                                ],  
+                              ),
+                              
+                            ),
+                            
                           ),
+                          
                         ),
-                      ),
+                        const Divider(
+                                      height: 25,
+                                      thickness: 1,
+                                      indent: 8,
+                                      endIndent: 10,
+                                      color: Colors.grey),
+                      ],
                     );
                   },
                   ),
